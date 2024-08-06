f4709ad5faa63749a7710cd87c6600d8a01902f2
Tsabit Haqqi <t.haqqi@student.saedubai.com>
2024-08-06 19:04:46 +0400

    made files for each work and created testing player

---
+++ GAD176.2_24T2/Assets/Haqqi's Work/Scripts/PlayerTestMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTestMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Speed of the player
+    private Rigidbody2D rb; // Reference to the Rigidbody2D component
+    private Vector2 movement; // Variable to store the movement input
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component attached to the player
+    }
+
+    void Update()
+    {
+        // Get the horizontal and vertical input (WASD or arrow keys)
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        // Store the movement input in the movement variable
+        movement = new Vector2(moveX, moveY).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        // Move the player using the Rigidbody2D component
+        rb.velocity = movement * moveSpeed;
+    }
+}
