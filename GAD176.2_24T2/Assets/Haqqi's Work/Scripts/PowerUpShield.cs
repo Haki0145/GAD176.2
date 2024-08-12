e33e94f0eaf9738f80c15bf3ca6f966b7655c163
Tsabit Haqqi <t.haqqi@student.saedubai.com>
2024-08-12 13:24:55 +0400

    added power up shield

---
+++ GAD176.2_24T2/Assets/Haqqi's Work/Scripts/PowerUpShield.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpShield : MonoBehaviour, IPowerUp
+{
+    public GameObject shieldPrefab; // The shield prefab to be activated
+    [SerializeField] float shieldDuration = 5f; // Duration of the shield
+
+    public void ExecutePowerUp(GameObject player)
+    {
+        // Check if the player already has a shield
+        if (player.transform.Find(shieldPrefab.name) == null)
+        {
+            // Activate the shield
+            GameObject shieldInstance = Instantiate(shieldPrefab, player.transform);
+            shieldInstance.name = shieldPrefab.name; // Set the name to avoid duplicates
+
+            // Position the shield at the player's position
+            shieldInstance.transform.localPosition = Vector3.zero;
+
+            // Start the coroutine to deactivate the shield after the duration
+            player.GetComponent<MonoBehaviour>().StartCoroutine(DeactivateShieldAfterTime(shieldInstance, shieldDuration));
+        }
+
+        // Optionally destroy the power-up object after it's used
+        Destroy(gameObject);
+    }
+
+    private IEnumerator DeactivateShieldAfterTime(GameObject shield, float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        Destroy(shield); // Destroy the shield after the duration
+    }
+}
